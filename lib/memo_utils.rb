4d7f7d1931f8c7e2887739d650fd77d42db310e6
Thomas Countz <thomascountz@gmail.com>
2025-02-09 23:36:45 +0100

    update copy memo rake task

--- lib/memo_utils.rb
+++ lib/memo_utils.rb
@@ -8,23 +8,59 @@ end
 
 # Extracts the title from the filename by removing the date and file extension, and converting underscores or hyphens to spaces
 def extract_title_from_filename(filename)
-  File.basename(filename, File.extname(filename)).split("-", 3).last.gsub(/[_-]/, " ").split.map(&:capitalize).join(" ")
+  File.basename(filename, ".*")
+    .sub(/^\d{4}-\d{2}-\d{2}-/, "")
+    .gsub(/[_-]/, " ")
+    .split
+    .map(&:capitalize)
+    .join(" ")
 end
 
-# Copies images referenced in the markdown content to the destination directory and updates the paths in the content
-def copy_images_and_update_paths(content, source_dir, dest_dir)
-  content.gsub(/!\[.*?\]\((.*?)\)/) do |match|
-    image_path = $1
+def extract_image_tags(content)
+  image_tag_pattern = /!\[(.*?)\]\((.*?)\)|!\[\[(.*?)\]\]/
+  content.scan(image_tag_pattern)
+end
+
+def copy_image(source_image_path, dest_image_path)
+  if File.exist?(source_image_path)
+    FileUtils.cp(source_image_path, dest_image_path)
+    true
+  else
+    false
+  end
+end
+
+def update_image_paths(content, source_dir, dest_dir)
+  image_tag_pattern = /!\[(.*?)\]\((.*?)\)|!\[\[(.*?)\]\]/
+
+  content.gsub(image_tag_pattern) do |match|
+    alt_text = $1 || $3
+    image_path = $2 || $3
+
+    if image_path.nil? || image_path.empty?
+      puts "Warning: Image path is missing in the tag: #{match}"
+      next match
+    end
+
+    # Handle cases where the image markdown contains a width specification
+    # e.g. ![[image.png|600]]
+    image_path = image_path.split("|")[0]
+    alt_text = alt_text.split("|")[0]
+
     source_image_path = File.join(source_dir, image_path)
-    if File.exist?(source_image_path)
-      dest_image_path = File.join(dest_dir, File.basename(image_path))
-      FileUtils.cp(source_image_path, dest_image_path)
-      "![#{File.basename(image_path)}](#{dest_image_path})"
+    dest_image_path = File.join(dest_dir, File.basename(image_path))
+
+    if copy_image(source_image_path, dest_image_path)
+      "![#{alt_text}](/#{dest_image_path})"
     else
       puts "Warning: Image file #{source_image_path} does not exist."
       match
     end
   end
+end
+
+def copy_images_and_update_paths(content, source_dir, dest_dir)
+  update_image_paths(content, source_dir, dest_dir)
 end
 
 # Reads the content of the file and removes any existing front matter
